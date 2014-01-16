dedc17cad8ac555685221ac054c05c31459c07d9
hpabst <hpabst@ualberta.ca>
2014-01-16 16:29:39 -0700

    Added TweetListModel and NormalTweetModel.java

---
+++ src/ca/ualberta/cs/lonelytwitter/NormalTweetModel.java
@@ -0,0 +1,28 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class NormalTweetModel extends LonelyTweetModel
+{
+
+	public NormalTweetModel(String text)
+	{
+
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Date getTimestamp()
+	{
+
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public boolean isImportant(){
+		return false;
+	}
+
+}
