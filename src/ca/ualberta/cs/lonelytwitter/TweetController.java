c5193ad62b1a6bd55fc9dd61bbb4f83f79b6d02e
hpabst <hpabst@ualberta.ca>
2014-01-16 16:46:02 -0700

    Added TweetController and TweetControllerInterface.

---
+++ src/ca/ualberta/cs/lonelytwitter/TweetController.java
@@ -0,0 +1,23 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.ArrayList;
+
+
+public class TweetController implements TweetControllerInterface
+{
+	private TweetListModel tweetListModel;
+
+	public TweetController()
+		{
+
+			super();
+			tweetListModel = new TweetListModel();
+		}
+
+	public void addTweet(String text)
+	{
+		ArrayList<AbstractLonelyTweetModel> list = tweetListModel.getTweetList();
+		list.add(new NormalTweetModel(text));
+	}
+
+}
